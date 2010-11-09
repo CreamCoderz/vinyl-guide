2d3f16f4e3f6c5e1dc0b9c7bac140f8b4bc27bf1
will <will@wills-mac-mini.local>
2010-11-08 21:52:56 -0800

    environment now loads properties from the rails root.

--- config/environment.rb
+++ config/environment.rb
@@ -42,6 +42,6 @@ Rails::Initializer.run do |config|
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   # config.i18n.default_locale = :de
 
-  PROPERTIES = YAML.load_file("config/build.#{Rails.env.downcase}.yml")
+  PROPERTIES = YAML.load_file("#{Rails.root}/config/build.#{Rails.env.downcase}.yml")
 
 end
