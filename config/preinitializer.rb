418c33e9dc68de2fd36e94d3fb08e9148cf07afe
Will Sulzer <will@Will-Sulzers-MacBook-Pro.local>
2011-05-03 23:00:13 -0700

    no longer need to use 'bundle exec' for everything. Bundler.setup (amongst other things) is called in the preinitializer.

---
+++ config/preinitializer.rb
@@ -0,0 +1,12 @@
+require "rubygems"
+require "bundler"
+
+if Gem::Version.new(Bundler::VERSION) <= Gem::Version.new("1.0")
+  raise RuntimeError, "Your bundler version is too old." +
+      "Run `gem install bundler` to upgrade."
+end
+
+# Set up load paths for all bundled gems
+ENV["BUNDLE_GEMFILE"] = File.expand_path("../../Gemfile", __FILE__)
+
+Bundler.setup
