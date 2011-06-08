33594eea93728b0234abc77ed72e674f963a1244
Will Sulzer <will@Will-Sulzers-MacBook-Pro.local>
2011-06-07 22:32:54 -0700

    using before_save instead of overriding method before_save

--- app/models/label.rb
+++ app/models/label.rb
@@ -2,9 +2,7 @@ class Label < ActiveRecord::Base
   has_many :releases
   validates_uniqueness_of :name, :case_sensitive => false, :message => "The label name must be unique."
 
-  def before_save
-    self.name = self.name.strip
-  end
+  before_save  lambda { self.name = self.name.strip}
 
   def ebay_item
     releases.first.ebay_items.first unless releases.empty?
