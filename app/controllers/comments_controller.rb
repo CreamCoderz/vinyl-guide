483f66b595059125400fd7c2a1c1338cc1db8db7
will <willsulzer@gmail.com>
2012-12-07 00:03:05 -0800

    Modified comments index view to display ebay items sorted by recency

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -25,7 +25,7 @@ class CommentsController < ApplicationController
   end
 
   def index
-    @comments = Comment.order('created_at DESC').paginate(:page => params[:page], :per_page => 50)
+    @comments = Comment.includes(:parent).order('comments.created_at DESC').paginate(:page => params[:page], :per_page => 50)
   end
 
   def show
